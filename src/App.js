309a89bd1a28ec1d7358eac3afa65d05281ef7db
1willcobb <cobb.will@gmail.com>
2023-08-08 15:52:24 +0000

    fixed vutton

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import './App.css';
+import React from 'react';
+import Button from './componets/Button.tsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React YES!
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <h1>My App</h1>
+            <Button/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/componets/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button>hey</button>
+    )
+}
+
+export default Button;
